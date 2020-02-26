03f189de3285fc08030a05e537a92ae1e21a28b5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-02-26 22:22:47 +0100

    j6primelte: inherit from common tree

---
+++ BoardConfig.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/j6primelte
+
+# Inherit from common msm8917-common
+-include device/samsung/msm8917-common/BoardConfigCommon.mk
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+# Kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := j6primelte
---
+++ omni_j6primelte.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from msm8917-common
+$(call inherit-product, device/samsung/msm8917-common/msm8917.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := j6primelte
+PRODUCT_NAME := omni_j6primelte
+PRODUCT_MODEL := Samsung Galaxy J6+
+PRODUCT_RELEASE_NAME := Samsung Galaxy J6+
+
